dcf25b902793da2540d5c47ca6ed5df79adbdd43
cdr <support@hildeio.com>
2024-01-20 00:28:46 +0100

    Redesign

--- hildeio_community/src/main/java/com/hildeio/HildeIoApplication.java
+++ hildeio_community/src/main/java/com/hildeio/HildeIoApplication.java
@@ -1,6 +1,6 @@
 package com.hildeio;
 
-import org.springframework.beans.BeansException; 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
