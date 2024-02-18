6ccdd89fcedac293ef6b22cc8316288f918e554c
cdr <support@hildeio.com>
2024-02-18 19:55:23 +0100

    Update Javadoc

--- hildeio_community/src/main/java/com/hildeio/homematic/HmEventManager.java
+++ hildeio_community/src/main/java/com/hildeio/homematic/HmEventManager.java
@@ -11,7 +11,7 @@ import com.hildeio.models.HmRequestModel;
 import com.hildeio.models.HmResponseModel;
 
 /***********************************************************************************************
- * Klasse HmEventManager1
+ * Klasse HmEventManager
  ***********************************************************************************************/
 public class HmEventManager {
 	
